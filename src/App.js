8ccd7d886a22eb3eae8f2fa2a3704ff6d017d8c2
madafakaoficial <sebastiangodoy45@hotmail.com>
2020-04-17 23:32:40 -0400

    2.0 Version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
-
+import ListadoImagenes from "./components/ListadoImagenes";
 function App() {
-  return (
-      <div className="container">
-        <div className="jumbotron">
-          <p className="lead text-center">Buscador de imagenes</p>
+  //state de la app
+  const [busqueda, guardarBusqueda] = useState("");
+  const [imagenes, guardarImagenes] = useState([]);
+  //states para el paginador
+  const [paginaactual, guardarPaginaActual] = useState(1);
+  const [totalpaginas, guardarTotalPaginas] = useState(1);
 
-          <Formulario/>
-        </div>
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (busqueda === "") return;
+
+      const imagenesPorPagina = 30;
+      const key = "16093699-d634adb6734b2808f5f8b9c82";
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaactual}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarImagenes(resultado.hits);
+
+      //calcular el total de paginas
+      const calcularTotalPaginas = Math.ceil(
+        resultado.totalHits / imagenesPorPagina
+      );
+      guardarTotalPaginas(calcularTotalPaginas);
+      //mover la pantalla hacia arriba, jumbo lo definimos en un div superios
+      const jumbotron = document.querySelector(".jumbotron");
+      jumbotron.scrollIntoView({ behavior: "smooth" });
+    };
+    consultarAPI();
+    //pasamos como dep pagina actual asi se recarga cuando cambiemos page
+  }, [busqueda, paginaactual]);
+
+  // definir la pagina anterior
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaactual - 1;
+    //esto para evitar ir a pagina -1
+    if (nuevaPaginaActual === 0) return;
+    guardarPaginaActual(nuevaPaginaActual);
+  };
+
+  //definir pagina siguiente
+  const paginaSiguiente = () => {
+    const nuevaPaginaActual = paginaactual + 1;
+
+    if (nuevaPaginaActual > totalpaginas) return;
+
+    guardarPaginaActual(nuevaPaginaActual);
+  };
+
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de imagenes</p>
+
+        <Formulario guardarBusqueda={guardarBusqueda} />
       </div>
+      <div className="row justify-content-center">
+        <ListadoImagenes imagenes={imagenes} />
+        {paginaactual === 1 ? null : (
+          <button
+            type="button"
+            className="bbtn btn-info mr-1"
+            onClick={paginaAnterior}
+          >
+            &laquo; Anterior{" "}
+          </button>
+        )}
+
+        {paginaactual === totalpaginas ? null : (
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={paginaSiguiente}
+          >
+            Siguiente &raquo;
+          </button>
+        )}
+      </div>
+    </div>
   );
 }
 
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,32 +1,46 @@
 //lo que el usuario pondra en el input , deberemos almacenarlo en el state
-import React,{useState} from 'react';
+import React, { useState } from "react";
+import Error from "./Error";
 
-const Formulario = () => {
-    
-    const [termino,guardarTermino] = useState('');
+const Formulario = ({ guardarBusqueda }) => {
+  const [termino, guardarTermino] = useState("");
+  const [error, guardarError] = useState(false);
 
-    return ( 
-        <form>
-            <div className="row">
-                <div className="form-group col-md-8">
-                    <input 
-                    type="text"
-                    className="form-control form-control-lg"
-                    placeholder="Busca una imagen,ejemplo: futbol"
-                    onChange={e => guardarTermino(e.target.value)}
-                    />
-                </div>
+  const buscarImagenes = (e) => {
+    e.preventDefault();
+    //validar
+    if (termino.trim() === "") {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+    //enviar termino de busqueda hacia el componente principal
+    guardarBusqueda(termino);
+  };
 
-                <div className="form-group col-md-4">
-                <input 
-                type="submit"
-                className="btn btn-lg btn-danger btn-block"
-                value="Buscar"
-                />
-            </div>
-            </div>
-        </form>
-     );
-}
- 
+  return (
+    <form onSubmit={buscarImagenes}>
+      <div className="row">
+        <div className="form-group col-md-8">
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            placeholder="Busca una imagen,ejemplo: futbol"
+            onChange={(e) => guardarTermino(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group col-md-4">
+          <input
+            type="submit"
+            className="btn btn-lg btn-danger btn-block"
+            value="Buscar"
+          />
+        </div>
+      </div>
+      {error ? <Error mensaje="Agrega un término de busqueda!" /> : null}
+    </form>
+  );
+};
+
 export default Formulario;
